6d7c3a81b5eddf8b07e4ffc7a8b54a4a6ed2151d
DevCurry <devcurry@a2zknowledgevisuals.com>
2013-04-09 23:19:12 +0530

    Code Commit

---
+++ MVC40_DynamicUI_Generation_Using_KnockoutJS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC40_DynamicUI_Generation_Using_KnockoutJS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVC40_DynamicUI_Generation_Using_KnockoutJS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC40_DynamicUI_Generation_Using_KnockoutJS.MvcApplication" Language="C#" %>
